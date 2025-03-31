2eaeedc1f23d468ff6a86146701a99c2503a78ca
Yan <yanfelixpimentell@gmail.com>
2025-03-31 13:42:42 -0300

    Correção de erros

--- src/Interfaces/AlunoInterface.java
+++ src/Interfaces/AlunoInterface.java
@@ -1,23 +1,7 @@
 package Interfaces;
-
-
-//import java.util.List;
-//
-////TODO FALTA MUDAR PRA REPOSITORIO DE ALUNOS
-//
-//
-
 import Entites.Aluno;
 import java.util.List;
 
-//// Interface para definir contrato de repositórios
-//
-//public interface RepositorioAlunoInterface<Aluno>{
-//    void adicionar(Aluno item);
-//    void remover (Aluno item);
-//    List<Aluno> listar();
-//
-//}
 
 
 // Interface para serviços do Aluno
